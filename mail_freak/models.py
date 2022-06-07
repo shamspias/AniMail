aeb46ba9541a054386a905f1201be752cf1f68fb
shamspias <shamsuddinpias0@gmail.com>
2022-06-07 17:09:01 +0600

    Add Models

--- mail_freak/models.py
+++ mail_freak/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Senders(models.Model):
+    """
+    List of all Senders
+    """
+    name = models.CharField(max_length=250, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class EmailTemplates(models.Model):
+    """
+    Basic Email send templates
+    """
+    name = models.CharField(max_length=20, blank=False, null=True)
+    subject = models.TextField(black=True, null=True)
+    main_text = models.TextField(blank=True, null=True)
+    senders = models.ForeignKey(Senders, blank=True, null=True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
